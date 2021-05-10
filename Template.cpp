08c724d76a01e3abd4ed182412f744b619a8cec6
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2021-05-10 11:08:30 +0530

    Add files via upload

---
+++ Template.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//basic input and data type
+#define ll long long  
+#define loop(a,b) for(ll int i=a;i<b;i++)
+#define si(x) scanf("%d",&x)
+#define sl(x) scanf("%lld",&x)
+#define ss(s) scanf("%s",s)
+#define pi(x) printf("%d\n",x)
+#define pl(x) printf("%lld\n",x)
+#define ps(s) printf("%s\n",s)
+
+//debugging 
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+
+//vector functions
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+typedef pair<int, int>pii;
+typedef pair<ll, ll>pl;
+typedef vector<int>vi;
+typedef vector<ll>vl;
+typedef vector<pii>vpii;
+typedef vector<pl>vpl;
+typedef vector<vi>vvi;
+typedef vector<vl>vvl;
+// solution
+
+void solve()
+{
+
+}
+
+//main function
+int main()
+{
+    ll int t=1;
+    sl(t);
+    while(t--)
+    {
+        solve();
+    }
+ 
+   return(0);
+}
